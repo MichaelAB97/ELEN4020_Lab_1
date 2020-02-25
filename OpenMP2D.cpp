4b73f4d0a54492f717c098426ddeceb8e5473311
SheLovesCode <munyaradzi.diana@gmail.com>
2020-02-25 20:26:41 +0200

    Transferred 2D multiplication using OpenMP from original repo

---
+++ OpenMP2D.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <stdlib.h>
+#include <chrono>
+#include <omp.h>
+
+using namespace std;
+using namespace std::chrono;
+
+int main ()
+{   
+    //Starting the steady clock
+    std::chrono::time_point<std::chrono::steady_clock> startClock, endClock;
+    startClock = std::chrono::steady_clock::now();
+    
+    int N = 10;
+    int matA[N][N] = {};
+    int matB[N][N] = {};
+    int matC[N][N] = {};
+
+    //Populating A and B arrays with random values between 0 and 50 and 50 and 100 respectively
+    for (int i=0; i!=N; i++)
+    {
+        for (int j=0; j!=N; j++)
+        {
+            matA[i][j] = rand() % N+1;
+            matB[i][j] = rand() % N+1;
+        }
+    }
+
+    //Multiplying the 2 arrays and storing it in matrix C 
+    #pragma omp parallel shared(matA,matB,matC)
+    {
+        #pragma omp for schedule(static)
+            for (int i=0; i<N; i++)
+            {
+                for (int j=0;j<N; j++)
+                {
+                    matC[i][j] = matC[i][j] + (matA[i][j]*matB[i][j]);
+                }
+            }
+    }
+
+    //Displaying 2D Arrays
+    cout << "matA: " << endl;
+    for (int i=0; i!=N; i++)
+    {
+        for (int j=0; j!=N; j++)
+        {
+            cout << matA[i][j] << " ";
+        }
+        cout << endl; 
+    }
+
+    cout << "matB: " << endl;
+    for (int i=0; i!=N; i++)
+    {
+        for (int j=0; j!=N; j++)
+        {
+            cout << matB[i][j] << " ";
+        }
+        cout << endl; 
+    }
+
+    cout << endl;
+
+    cout << "matC: " << endl;
+    for (int i=0; i!=N; i++)
+    {
+        for (int j=0; j!=N; j++)
+        {
+            cout << matC[i][j] << " ";
+        }
+        cout << endl; 
+    }
+
+    endClock = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsedTime = duration_cast<duration<double>>(endClock - startClock);
+
+    cout << "Elapsed Time in Seconds: " << elapsedTime.count() << endl;
+
+    return 0;
+}
